80673147aee46b88867b31112fb57539432f1476
UDPxZOE <132156485+UDPxZOE@users.noreply.github.com>
2023-05-03 17:44:03 +0530

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,47 @@
+<?php
+if (isset($_POST["submit"])) {
+    $targetDirectory = "uploads/";  // Directory to store uploaded files
+    $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // Check if file is a valid image
+    if (isset($_FILES["fileToUpload"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+
+    // Check if file already exists
+    if (file_exists($targetFile)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+
+    // Check file size (limit it to 2MB)
+    if ($_FILES["fileToUpload"]["size"] > 2 * 1024 * 1024) {
+        echo "Sorry, your file is too large. It should be under 2MB.";
+        $uploadOk = 0;
+    }
+
+    // Limit allowed file types to images only (you can modify it for different file types)
+    if ($fileType !== "jpg" && $fileType !== "png" && $fileType !== "jpeg" && $fileType !== "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // If all checks pass, upload the file
+    if ($uploadOk == 1) {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
+?>
